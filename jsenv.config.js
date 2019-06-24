0b76bba6058d8e567bb8b1355cfef8385e08b64b
Damien Maillard <damien.maillard@dailymotion.com>
2019-06-24 17:48:06 +0200

    run test only on node

--- jsenv.config.js
+++ jsenv.config.js
@@ -1,25 +1,10 @@
-const { launchNode, launchChromium } = require("@jsenv/core")
+const { launchNode } = require("@jsenv/core")
 
 const projectPath = __dirname
 exports.projectPath = projectPath
 
 const testDescription = {
   "/test/**/*.test.js": {
-    browser: {
-      launch: launchChromium,
-    },
-    node: {
-      launch: launchNode,
-    },
-  },
-  "/test/browser/**/*.test.js": {
-    browser: {
-      launch: launchChromium,
-    },
-    node: null,
-  },
-  "/test/node/**/*.test.js": {
-    browser: null,
     node: {
       launch: launchNode,
     },
